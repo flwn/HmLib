4842ade38f454f47cc51c27e17e3db79724b072f
Erik Aalbers <flwn@users.noreply.github.com>
2015-05-16 21:37:34 +0200

    Added test project.

--- src/HmLib.CommandLine/Program.cs
+++ src/HmLib.CommandLine/Program.cs
@@ -20,27 +20,6 @@ namespace HmLib.CommandLine
         }
         public static async Task Test()
         {
-            var testRequest = new Request()
-            {
-                Method = "system.listMethods",
-                Parameters = { "Bla" }
-            };
-            testRequest.SetAuthorization("wiki", "pedia");
-
-            var bufferStream = new MemoryStream();
-            new HmBinaryProtocol().WriteRequest(bufferStream, testRequest);
-            var buffer = bufferStream.ToArray();
-            var testoutput = new StringBuilder((int)buffer.Length * 2);
-
-            foreach (var byte1 in buffer)
-            {
-                testoutput.AppendFormat("{0:X2}", byte1);
-            }
-            var expected =
-                "42696E400000002F000000010000000D417574686F72697A6174696F6E0000001642617369632064326C72615470775A57527059513D3D000000250000001273797374656D2E6C6973744D6574686F6473000000010000000300000003426C61";
-
-            var success = expected == testoutput.ToString();
-
             var request = new Request()
             {
                 Method = "system.listMethods"
@@ -53,7 +32,6 @@ namespace HmLib.CommandLine
                 await client.ConnectAsync();
 
                 var respone = await client.ExecuteRequest(request);
-
             }
         }
     }
---
+++ src/HmLib.Tests/Binary/HmBinaryProtocolTests.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HmLib.Tests.Binary
+{
+    using HmLib.Binary;
+    using Shouldly;
+    using System.Globalization;
+
+    public class HmBinaryProtocolTests
+    {
+
+        public void ShouldWork()
+        {
+            var testRequest = new Request()
+            {
+                Method = "system.listMethods",
+                Parameters = { "Bla" }
+            };
+            testRequest.SetAuthorization("wiki", "pedia");
+
+            var bufferStream = new MemoryStream();
+            new HmBinaryProtocol().WriteRequest(bufferStream, testRequest);
+            var buffer = bufferStream.ToArray();
+            var testoutput = new StringBuilder(buffer.Length * 2);
+
+            foreach (var byte1 in buffer)
+            {
+                testoutput.AppendFormat("{0:X2}", byte1);
+            }
+            var expected =
+                "42696E400000002F000000010000000D417574686F72697A6174696F6E0000001642617369632064326C72615470775A57527059513D3D000000250000001273797374656D2E6C6973744D6574686F6473000000010000000300000003426C61";
+
+            testoutput.ToString().ShouldBe(expected);
+
+        }
+    }
+}
