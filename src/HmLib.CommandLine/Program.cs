4de4b7148fa7e6709e454b5b8565bf00069c528e
Erik Aalbers <flwn@users.noreply.github.com>
2015-06-15 19:30:38 +0200

    Factored RPC listener client into HmRpcServer.

--- src/HmLib.CommandLine/Program.cs
+++ src/HmLib.CommandLine/Program.cs
@@ -7,11 +7,15 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace HmLib.CommandLine
 {
+    using HmLib.Serialization;
     using Proxy;
     using Proxy.Devices;
+    using System.Net.Sockets;
+    using Switch = Proxy.Devices.Switch;
 
     public class Program
     {
@@ -28,24 +32,79 @@ namespace HmLib.CommandLine
         };
         public void Main(string[] args)
         {
-            Test().Wait();
-            //Console.ReadLine();
+
+            var listener = new HmRpcServer(req =>
+            {
+                return null;
+            });
+
+            listener.Start();
+
+            try
+            {
+                Test().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            //s.Wait();
+
+            Console.ReadLine();
+            listener.Dispose();
         }
         public static async Task Test()
         {
             var endpoint = new IPEndPoint(IPAddress.Parse("192.168.63.3"), 2001);
             using (var client = new HmRpcClient(endpoint))
             {
+                var stopwatch = new Stopwatch();
+
+                stopwatch.Start();
                 await client.ConnectAsync();
+                Console.WriteLine("Connect: {0}ms.", stopwatch.Elapsed.TotalMilliseconds);
 
                 var proxy = new GenericProxy(client);
 
+                var req1 = new Request { Method = "setValue", Parameters = { "HEQ0359881:1", "LEVEL", 1d, "FLOAT" } };
+                var req2 = new Request { Method = "setValue", Parameters = { "HEQ0359959:1", "LEVEL", 1d, "FLOAT" } };
+                var req3 = new Request { Method = "setValue", Parameters = { "IEQ0019224:1", "LEVEL", 0d, "FLOAT" } };
+                var req4 = new Request { Method = "setValue", Parameters = { "IEQ0020290:1", "LEVEL", 0d, "FLOAT" } };
+                var req5 = new Request { Method = "setValue", Parameters = { "IEQ0020353:1", "LEVEL", 0d, "FLOAT" } };
+
+                //var response = await proxy.MultiCall(req1, req2, req3, req4, req5);
+
+                var req1_ = new Request { Method = "setValue", Parameters = { "HEQ0359881:1", "LEVEL", 0d, "FLOAT" } };
+                var req2_ = new Request { Method = "setValue", Parameters = { "HEQ0359959:1", "LEVEL", 0d, "FLOAT" } };
+                var req3_ = new Request { Method = "setValue", Parameters = { "IEQ0019224:1", "LEVEL", 1d, "FLOAT" } };
+                var req4_ = new Request { Method = "setValue", Parameters = { "IEQ0020290:1", "LEVEL", 1d, "FLOAT" } };
+                var req5_ = new Request { Method = "setValue", Parameters = { "IEQ0020353:1", "LEVEL", 1d, "FLOAT" } };
+
+                //var response_ = await proxy.MultiCall(req1_, req2_, req3_, req4_, req5_);
+                //return;
+
+                var pong = await client.ExecuteRequest(new Request { Method = "init", Parameters = { "binary://192.168.63.192:6300", "TEST", 0 } });
+                Console.WriteLine(pong);
+
+                return;
+
+                await GatherInfo(proxy);
+                return;
+
+                var defBlind = Devices["blind_door"];
+                var blind = (Blind)Activator.CreateInstance(defBlind.Item2, defBlind.Item1, proxy);
+                stopwatch.Restart();
+                await blind.SetLevel(0.5d);
+                Console.WriteLine("Blind.SetLevel: {0}ms.", stopwatch.Elapsed.TotalMilliseconds);
+
+                Console.ReadLine();
+                Thread.Sleep(1000);
+                await blind.SetLevel(1d);
+                return;
 
                 var defDimmer = Devices["dimmer_dining"];
                 var dimmer = (Dimmer)Activator.CreateInstance(defDimmer.Item2, defDimmer.Item1, proxy);
 
-                var defBlind = Devices["blind_dining"];
-                var blind = (Blind)Activator.CreateInstance(defBlind.Item2, defBlind.Item1, proxy);
 
                 var defSwitch = Devices["switch_hallway"];
                 var @switch = (Switch)Activator.CreateInstance(defSwitch.Item2, defSwitch.Item1, proxy);
@@ -55,9 +114,6 @@ namespace HmLib.CommandLine
 
                 await @switch.SetState(false);
 
-                await blind.SetLevel(0.8d);
-                Thread.Sleep(500);
-                await blind.SetLevel(1d);
 
 
                 for (double i = 0; i <= 1; i += 0.1)
@@ -66,15 +122,22 @@ namespace HmLib.CommandLine
                     Thread.Sleep(500);
                 }
                 await dimmer.SetLevel(0d);
-                
-                //File.WriteAllText("methods.json", await proxy.ListMethods());
-                //File.WriteAllText("interfaces.json", await proxy.ListBidcosInterfaces());
-                //File.WriteAllText("devices.json", await proxy.ListDevices("HEQ0356495"));
+
 
                 //Console.WriteLine(response);
             }
 
         }
-            
+
+        public static async Task GatherInfo(GenericProxy proxy)
+        {
+            File.WriteAllText("serviceMessages.json", await proxy.GetServiceMessages());
+
+
+            //File.WriteAllText("methods.json", await proxy.ListMethods());
+            //File.WriteAllText("interfaces.json", await proxy.ListBidcosInterfaces());
+            //File.WriteAllText("devices.json", await proxy.ListDevices("HEQ0356495"));
+        }
+
     }
 }
---
+++ src/HmLib/HmRpcServer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HmLib
+{
+    public class HmRpcServer : IDisposable
+    {
+        private readonly Func<Request, Response> _requestHandler;
+        private readonly TcpListener _listener;
+
+        private ManualResetEventSlim _acceptWaiter = new ManualResetEventSlim();
+
+        private Task _listenerTask;
+        private readonly List<TcpClient> _connections = new List<TcpClient>();
+
+        public HmRpcServer(Func<Request, Response> requestHandler)
+        {
+            _requestHandler = requestHandler;
+
+            _listener = new TcpListener(IPAddress.Any, 6300);
+            _listener.Server.ReceiveTimeout = 3000 * 10;
+        }
+
+
+        public void Start()
+        {
+            if (_listenerTask != null)
+            {
+                return;
+            }
+
+            _listener.Start();
+            _listenerTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var connection = await _listener.AcceptTcpClientAsync();
+
+                    var task = StartHandleConnectionAsync(connection);
+                    // if already faulted, re-throw any error on the calling context
+                    if (task.IsFaulted)
+                        task.Wait();
+                }
+            });
+        }
+        // Handle new connection
+        private async Task HandleConnectionAsync(TcpClient tcpClient)
+        {
+            var local = (IPEndPoint)tcpClient.Client.LocalEndPoint;
+            var remote = (IPEndPoint)tcpClient.Client.RemoteEndPoint;
+            Console.Write("Incoming! (Local={0}, Remote=", local);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(remote);
+            Console.ResetColor();
+            Console.WriteLine(")");
+            await Task.Yield();
+            // continue asynchronously on another threads
+
+            using (var stream = tcpClient.GetStream())
+            {
+                var protocol = new HmLib.Binary.HmBinaryProtocol();
+
+                var alreadyWrittenToResponse = false;
+                try
+                {
+                    var request = protocol.ReadRequest(stream);
+                    var response = (object)_requestHandler(request);
+
+                    Console.WriteLine(request);
+
+                    switch (request.Method)
+                    {
+                        case "system.listMethods":
+                            response = new List<object> { "system.multicall" };
+                            break;
+                        case "system.multicall":
+                        default:
+                            response = null;
+                            break;
+                    }
+
+                    //buffer for robustness.
+                    using (var buffer = new MemoryStream())
+                    {
+                        protocol.WriteResponse(buffer, response);
+                        alreadyWrittenToResponse = true;
+                        await buffer.CopyToAsync(stream);
+                    }
+                }
+                catch (ProtocolException protocolException)
+                {
+                    if (!alreadyWrittenToResponse)
+                    {
+                        //do not write error if already written to stream...
+                        protocol.WriteResponse(stream, protocolException);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: ", ex);
+                }
+            }
+        }
+        private async Task StartHandleConnectionAsync(TcpClient tcpConnection)
+        {
+
+            // start the new connection task
+            var connectionTask = HandleConnectionAsync(tcpConnection);
+
+            // catch all errors of HandleConnectionAsync
+            try
+            {
+                await connectionTask;
+                // we may be on another thread after "await"
+            }
+            catch (Exception ex)
+            {
+                // log the error
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        #region IDisposable Support
+        private bool isDisposed = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    _listener.Stop();
+
+
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                isDisposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+
+
+    }
+}
