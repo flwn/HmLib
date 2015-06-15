4901d0fc99fafd1a3dcb074b2fa0f9e625fc2dce
Erik Aalbers <flwn@users.noreply.github.com>
2015-06-15 11:22:17 +0200

    Extended proxy with extra methods.

--- src/HmLib.Proxy/GenericProxy.cs
+++ src/HmLib.Proxy/GenericProxy.cs
@@ -32,30 +32,50 @@ namespace HmLib.Proxy
             if (string.IsNullOrWhiteSpace(paramsetKey)) throw new ArgumentNullException("paramsetKey");
             return await ExecuteMethod("getParamset", address, paramsetKey);
         }
+
+        public async Task SetValue(string address, string key, bool value)
+        {
+            await SetValue(address, key, value, "BOOL");
+        }
+
+        public async Task SetValue(string address, string key, int value)
+        {
+            await SetValue(address, key, value, "INTEGER");
+        }
+
+        public async Task SetValue(string address, string key, double value)
+        {
+            await SetValue(address, key, value, "FLOAT");
+        }
+        public async Task SetValue(string address, string key, string value)
+        {
+            await SetValue(address, key, value, "STRING");
+        }
+
         public async Task SetValue(string address, string key, object value, string type)
         {
             if (string.IsNullOrWhiteSpace(address)) throw new ArgumentNullException("address");
             if (string.IsNullOrWhiteSpace(type)) throw new ArgumentNullException("type");
 
+            type = type.ToUpper();
             if (type == "FLOAT")
             {
-                if (!(value is double)) throw new ArgumentException("value must be of type double", "value");
+                if (!(value is double)) throw new ArgumentException("value must be of type Double", "value");
             }
             else if (type == "BOOL")
             {
-                if (!(value is bool)) throw new ArgumentException("value must be of type boolean", "value");
+                if (!(value is bool)) throw new ArgumentException("value must be of type Boolean", "value");
             }
-            else
+            else if (type == "STRING")
             {
-                throw new NotSupportedException("Types other than FLOAT are not supported.");
+                if (!(value is string)) throw new ArgumentException("value must be of type String", "value");
+            }
+            else if (type == "INTEGER" || type == "ENUM")
+            {
+                if (!(value is int)) throw new ArgumentException("value must be of type Int32", "value");
             }
 
             await ExecuteMethod("setValue", address, key, value, type);
-        }
-        public async Task<string> MethodHelp(string methodName)
-        {
-            if (string.IsNullOrWhiteSpace(methodName)) throw new ArgumentNullException("methodName");
-            return await ExecuteMethod("system.methodHelp", methodName);
         }
 
         public async Task<string> ListDevices(string interfaceId)
@@ -68,9 +88,31 @@ namespace HmLib.Proxy
         {
             return await ExecuteMethod("listBidcosInterfaces");
         }
+
+        public async Task<string> GetServiceMessages()
+        {
+            return await ExecuteMethod("getServiceMessages");
+        }
         public async Task<string> ListMethods()
         {
             return await ExecuteMethod("system.listMethods");
+        }
+        public async Task<string> MethodHelp(string methodName)
+        {
+            if (string.IsNullOrWhiteSpace(methodName)) throw new ArgumentNullException("methodName");
+            return await ExecuteMethod("system.methodHelp", methodName);
+        }
+
+        public async Task<string> MultiCall(params Request[] requests)
+        {
+            var multiCallParams = requests
+                    .Select(x => new Dictionary<string, object> {
+                        { "methodName", x.Method },
+                        { "params", x.Parameters }
+                    })
+                    .ToList();
+
+            return await ExecuteMethod("system.multicall", multiCallParams);
         }
 
         protected virtual async Task<string> ExecuteMethod(string method, params object[] parameters)
@@ -84,5 +126,7 @@ namespace HmLib.Proxy
 
             return response.Content;
         }
+
+
     }
 }
