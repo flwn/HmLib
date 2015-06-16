45ce0c18d7cc9184953f35e6ac249453c960e72a
Erik Aalbers <flwn@users.noreply.github.com>
2015-06-16 12:56:06 +0200

    Added tests for HmBinaryProtocol.

--- src/HmLib.Tests/Binary/HmBinaryProtocolTests.cs
+++ src/HmLib.Tests/Binary/HmBinaryProtocolTests.cs
@@ -9,12 +9,13 @@ using System.Threading.Tasks;
 namespace HmLib.Tests.Binary
 {
     using HmLib.Binary;
+    using HmLib.Serialization;
     using Shouldly;
 
     public class HmBinaryProtocolTests
     {
 
-        public void ShouldWork()
+        public void ShouldWriteRequestsCorrectly()
         {
             var testRequest = new Request()
             {
@@ -24,7 +25,8 @@ namespace HmLib.Tests.Binary
             testRequest.SetAuthorization("wiki", "pedia");
 
             var bufferStream = new MemoryStream();
-            new HmBinaryProtocol().WriteRequest(bufferStream, testRequest);
+            var protocol = new HmBinaryProtocol();
+            protocol.WriteRequest(bufferStream, testRequest);
             var buffer = bufferStream.ToArray();
             var testoutput = new StringBuilder(buffer.Length * 2);
 
@@ -37,6 +39,60 @@ namespace HmLib.Tests.Binary
 
             testoutput.ToString().ShouldBe(expected);
 
+        }
+
+        public void ShouldReadRequestsCorrectly()
+        {
+            var protocol = new HmBinaryProtocol(() => new ObjectBuilder());
+
+            var requestByteString =
+                "42696E400000002F000000010000000D417574686F72697A6174696F6E0000001642617369632064326C72615470775A57527059513D3D000000250000001273797374656D2E6C6973744D6574686F6473000000010000000300000003426C61";
+
+            var requestBytes = Enumerable.Range(0, requestByteString.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(requestByteString.Substring(x, 2), 16))
+                     .ToArray();
+
+            var requestStream = new MemoryStream(requestBytes);
+            var request = protocol.ReadRequest(requestStream);
+
+            request.ShouldNotBe(null);
+            request.Method.ShouldBe("system.listMethods");
+            request.Parameters.Count.ShouldBe(1);
+            request.Parameters.First().ShouldBe("Bla");
+        }
+
+        public void BinaryWriterSupportsObjectBuilderInterface()
+        {
+
+            var output = new MemoryStream();
+            var protocol = new HmBinaryProtocol(() => new HmBinaryWriter(output));
+
+            var requestByteString =
+                "42696E400000002F000000010000000D417574686F72697A6174696F6E0000001642617369632064326C72615470775A57527059513D3D000000250000001273797374656D2E6C6973744D6574686F6473000000010000000300000003426C61";
+
+
+            var requestBytes = Enumerable.Range(0, requestByteString.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(requestByteString.Substring(x, 2), 16))
+                     .ToArray();
+
+            var requestStream = new MemoryStream(requestBytes);
+            var request = protocol.ReadRequest(requestStream);
+
+            var outputBuffer = output.ToArray();
+            var outputFormatted = string.Join("", outputBuffer.Select(x => x.ToString("X2")));
+
+
+            //value         hex
+            //----------------------
+            //arrayType     00000100
+            //arrayCount    00000001
+            //stringType    00000003
+            //stringLength  00000003
+            //Bla           42 6C 61
+
+            outputFormatted.ShouldBe("00000100000000010000000300000003426C61");
         }
     }
 }
